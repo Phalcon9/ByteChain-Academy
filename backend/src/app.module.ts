9d17d7fe603e7e28096b4f6fe884f92f3b394077
Rukayat Zakariyau <ruke4k4@gmail.com>
2025-03-22 03:41:25 -0700

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,20 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres', // Change this to your DB type
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '123456',
-    database: 'nestjs-blog',
-    autoLoadEntities: true,
-    synchronize: true, // ⚠️ Set false in production
-  }),StudentModule],
+  imports:[StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
